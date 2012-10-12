dffb4372fbed12b603a4fd924c82b90ac8906580
S. Andrew Sheppard <andrew@wq.io>
2012-10-12 13:25:16 -0500

    wait to load jQM until routers have attached to mobileinit

--- js/lib/jquery.mobile.js
+++ js/lib/jquery.mobile.js
@@ -12,7 +12,7 @@
 (function ( root, doc, factory ) {
 	if ( typeof define === "function" && define.amd ) {
 		// AMD. Register as an anonymous module.
-		define( [ "./jquery" ], function ( $ ) {
+		define( [ "./jquery", "./jquery.mobile.router", "../router" ], function ( $ ) {
 			factory( $, root, doc );
 			return $.mobile;
 		});
