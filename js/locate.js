7acf8a204337a10fb35128aaca96809382e3d009
S. Andrew Sheppard <andrew@wq.io>
2013-04-22 22:34:38 -0500

    support watch option in locate

--- js/locate.js
+++ js/locate.js
@@ -18,9 +18,10 @@ locate.init = function(map) {
 }
 
 // Simple geolocation function
-locate.locate = function(success, error, high) {
+locate.locate = function(success, error, high, watch) {
     var map = _map || L.map(L.DomUtil.create('div'));
     var opts = {}; 
+    var nospin = false;
 
     // If no success callback, assume setView
     if (!success) {
@@ -32,20 +33,33 @@ locate.locate = function(success, error, high) {
         opts['enableHighAccuracy'] = true;
         opts['timeout'] = 60 * 1000;
     }
+    if (watch) {
+        opts['watch'] = true;
+        nospin = true;
+    }
 
     map.off('locationfound');
     map.off('locationerror');
     map.on('locationfound', go(success));
     map.on('locationerror', go(error));
 
-    spin.start();
-    map.locate(opts)
+    if (!nospin)
+        spin.start();
+    map.locate(opts);
     function go(fn) {
         return function(evt) {
-            spin.stop();
+            if (!nospin)
+                spin.stop();
             fn(evt);
         }
     }
+
+    if (watch)
+        return {
+            'stop': function() {
+                map.stopLocate();
+            }
+        }
 }
 
 return locate;
