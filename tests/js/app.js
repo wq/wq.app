eb9e45a41cc3138a842a04a9bafb10ec8c3bbb6d
S. Andrew Sheppard <andrew@wq.io>
2013-11-07 16:23:18 -0600

    update test app

--- tests/js/app.js
+++ tests/js/app.js
@@ -1,12 +1,13 @@
 requirejs.config({
+    'baseUrl': '../../js',
     'paths': {
-        'wq': '../../js'
+        'app': '../tests/js/app'
     }
 });
 
 var baseurl = window.location.pathname.replace(/\/$/,'');
 
-require(['wq/app', 'wq/map', './config', './templates'],
+require(['wq/app', 'wq/map', 'app/config', 'app/templates'],
 function(app, map, config, templates) {
     app.init(config, templates, baseurl);
     map.init(config.map);
