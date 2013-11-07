5b16c702041163857210ef3c2c7bf4ae8323573a
S. Andrew Sheppard <andrew@wq.io>
2013-11-07 16:45:34 -0600

    update wq init to new structure

--- build/init.py
+++ build/init.py
@@ -5,7 +5,7 @@ def init(conf, indir):
     jsdir = conf.get('js', indir + '/js')
     cssdir = conf.get('css', indir + '/css')
     assets = os.path.dirname(os.path.dirname(__file__))
-    if not os.path.exists(jsdir + '/wq'):
-        os.symlink(assets + '/js', jsdir + '/wq')
-    if not os.path.exists(cssdir + '/wq'):
-        os.symlink(assets + '/css', cssdir + '/wq')
+    if not os.path.exists(jsdir + '/lib'):
+        os.symlink(assets + '/js', jsdir + '/lib')
+    if not os.path.exists(cssdir + '/lib'):
+        os.symlink(assets + '/css', cssdir + '/lib')
