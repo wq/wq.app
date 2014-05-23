12c88b132a0b9da0f3082dbd7cfb2cea819f3d11
S. Andrew Sheppard <andrew@wq.io>
2014-05-23 12:15:19 -0500

    simple ajax polling for <progress> element

---
+++ js/wq/progress.js
@@ -0,0 +1,91 @@
+/*
+ * wq.app 0.6.0-dev - progress.js
+ * Simple AJAX polling for HTML5 <progress> element
+ * (c) 2014, S. Andrew Sheppard
+ * http://wq.io/license
+ */
+
+define(['./pages', './json'], function(pages, json) {
+
+// Exported module variable
+var progress = {
+    'interval': 0.5 // Polling interval (in seconds)
+};
+
+// Internal setInterval ids
+var _timers = {};
+
+// Optionally initialize wq/progress with a page path to auto start polling
+progress.init = function(path, onComplete) {
+    pages.addRoute(path, 's', _startProgress);
+    pages.addRoute(path, 'h', _stopProgress);
+    progress.onComplete = onComplete;
+
+    function _startProgress(match, ui, params, hash, evt, $page) {
+        progress.start($page.find('progress'));
+    }
+    function _stopProgress(match, ui, params, hash, evt, $page) {
+        progress.stop($page.find('progress'));
+    }
+};
+
+// progress.start accepts a jQuery-wrapped progress element and looks for a
+// data-url attribute to poll (and an optional data-interval attribute)
+progress.start = function($progress) {
+    var url = $progress.data('url');
+    var interval = $progress.data('interval') || progress.interval;
+    if (!url || _timers[url])
+        return;
+    _timers[url] = setInterval(
+        progress.timer($progress, url),
+        interval * 1000
+    );
+};
+
+// progress.stop stops a timer started with progress.start
+progress.stop = function($progress) {
+    var url = $progress.data('url');
+    if (!url || !_timers[url])
+        return;
+    clearInterval(_timers[url]);
+};
+
+// progress.complete stops a timer started with progress.start, with a hook for
+// custom response
+progress.complete = function($progress, data) {
+    progress.stop($progress);
+    if (progress.onComplete)
+        progress.onComplete($progress, data);
+};
+
+// progress.timer generates a function suitable for setInterval
+// (with $progress and url bound to scope).
+progress.timer = function($progress, url) {
+    return function() {
+        json.get(url, function(data) {
+            if (!data.total) {
+                // Set to "intermediate" state
+                $progress.attr('total', '');
+                $progress.attr('value', '');
+
+                // Fallback for old browsers
+                $progress.html('Loading...');
+            } else {
+                // Set to progress level
+                $progress.attr('value', data.current);
+                $progress.attr('max', data.total);
+
+                // Fallback for old browsers
+                $progress.html(data.current / data.total * 100 + "%");
+
+                // Check for completion
+                if (data.current == data.total || data.status == "SUCCESS")
+                    progress.complete($progress, data);
+            }
+        });
+    };
+};
+
+return progress;
+
+});
