7680da27e3eae68c58abadba047ebaae0516dd48
S. Andrew Sheppard <andrew@wq.io>
2014-06-03 15:35:19 -0500

    stop progress bar on failure

--- js/wq/progress.js
+++ js/wq/progress.js
@@ -17,10 +17,11 @@ var _timers = {};
 var _last = {};
 
 // Optionally initialize wq/progress with a page path to auto start polling
-progress.init = function(path, onComplete) {
+progress.init = function(path, onComplete, onFail) {
     pages.addRoute(path, 's', _startProgress);
     pages.addRoute(path, 'h', _stopProgress);
     progress.onComplete = onComplete;
+    progress.onFail = onFail;
 
     function _startProgress(match, ui, params, hash, evt, $page) {
         progress.start($page.find('progress'));
@@ -51,12 +52,18 @@ progress.stop = function($progress) {
     clearInterval(_timers[url]);
 };
 
-// progress.complete stops a timer started with progress.start, with a hook for
-// custom response
+// progress.complete/progress.fail stop a timer started with progress.start,
+// with a hook for custom response
 progress.complete = function($progress, data) {
     progress.stop($progress);
     if (progress.onComplete)
         progress.onComplete($progress, data);
+};
+
+progress.fail = function($progress, data) {
+    progress.stop($progress);
+    if (progress.onFail)
+        progress.onFail($progress, data);
 };
 
 // progress.timer generates a function suitable for setInterval
@@ -66,8 +73,8 @@ progress.timer = function($progress, url) {
         json.get(url, function(data) {
             if (!data.total) {
                 // Set to "intermediate" state
-                $progress.attr('total', '');
-                $progress.attr('value', '');
+                $progress.prop('total', null);
+                $progress.prop('value', null);
 
                 // Fallback for old browsers
                 $progress.html('Loading...');
@@ -78,8 +85,8 @@ progress.timer = function($progress, url) {
                     /* jshint noempty: false */
                 } else {
                     _last[url] = data.current;
-                    $progress.attr('value', data.current);
-                    $progress.attr('max', data.total);
+                    $progress.prop('value', data.current);
+                    $progress.prop('max', data.total);
 
                     // Fallback for old browsers
                     $progress.html(data.current / data.total * 100 + "%");
@@ -89,6 +96,8 @@ progress.timer = function($progress, url) {
                 if (data.current == data.total || data.status == "SUCCESS")
                     progress.complete($progress, data);
             }
+            if (data.status == "FAILURE")
+                progress.fail($progress, data);
         });
     };
 };
