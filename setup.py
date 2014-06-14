74115a9c7af439e18d3d2831e25ef06767383d22
S. Andrew Sheppard <andrew@wq.io>
2014-06-14 10:29:23 -0500

    v0.6.0

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ for folder in ['js', 'css', 'scss']:
 
 setup(
     name='wq.app',
-    version='0.6.0-dev',
+    version='0.6.0',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='http://wq.io/wq.app',
