5592630c85b1ead6de56f48e50712df8764c0200
S. Andrew Sheppard <andrew@wq.io>
2014-07-29 17:32:07 -0500

    wq/autocomplete.js: an AJAX-powered <datalist>

---
+++ js/wq/autocomplete.js
@@ -0,0 +1,104 @@
+/*
+ * wq.app 0.6.2-dev - autocomplete.js
+ * Simple AJAX autocomplete leveraging the HTML5 <datalist> element
+ * (c) 2014, S. Andrew Sheppard
+ * http://wq.io/license
+ */
+
+define(['jquery', './pages', './json', './template', './spinner'],
+function($, pages, json, tmpl, spin) {
+
+// Exported module variable
+var auto = {};
+
+auto.template = '{{#list}}<option value="{{id}}">{{label}}</option>{{/list}}';
+
+// Automatically register callbacks for every datalist
+auto.init = function(template) {
+    if (template)
+        auto.template = template;
+    pages.addRoute('.*', 's', _register);
+    function _register(match, ui, params, hash, evt, $page) {
+        $page.find('datalist').each(function(i, datalist) {
+            /* jshint unused: false */
+            auto.register($(datalist), $page);
+        });
+    }
+};
+
+// Register a jQuery-wrapped datalist element.  Changes to associated inputs
+// will trigger an update.
+auto.register = function($datalist, $scope) {
+    var $input;
+    if (!$scope)
+        $scope = $datalist.parents('body');
+    $input = $scope.find('input[list="' + $datalist.attr('id') + '"]');
+    $input.on('input', _update);
+
+    function _update() {
+        auto.update($datalist, $input.val());
+    }
+};
+
+var _cache = {};
+auto.update = function($datalist, value) {
+    var url = $datalist.data('url'),
+        param = $datalist.data('query') || 'q',
+        min = $datalist.data('min') || 3;
+
+    // Only continue if a long enough value is present
+    if (!value || value.length < min) {
+        return;
+    }
+
+    // Compute full query url & re-use cached version if present
+    url += url.indexOf('?') > -1 ? '&' : '?';
+    url += param + '=' + value;
+    if (_cache[url]) {
+        auto.render($datalist, _cache[url]);
+        return;
+    }
+
+    // Load results via AJAX
+    spin.start();
+    json.get(url, function(result) {
+        spin.stop();
+        if (!result.list)
+            result = {'list': result};
+        _cache[url] = result;
+        auto.render($datalist, result);
+    });
+
+};
+
+// Update <datalist> HTML with new <options>
+auto.render = function($datalist, result) {
+    var $options = $datalist.find('option'),
+        data = result.list,
+        $tmpdl, $tmpopts, last;
+
+    // Reset list if no data present
+    if (!data || !data.length) {
+        $datalist.empty();
+        return;
+    }
+
+    // Don't update if list is the same as before
+    if ($options.length == data.length) {
+        $tmpdl = $("<datalist>");
+        $tmpdl.append(tmpl.render(auto.template, result));
+        $tmpopts = $tmpdl.find('option');
+        last = data.length - 1;
+        if ($options[0].value == $tmpopts[0].value &&
+                $options[last].value == $tmpopts[last].value) {
+            return;
+        }
+    }
+
+    // Render data into template
+    $datalist.empty().append(tmpl.render(auto.template, result));
+};
+
+return auto;
+
+});
