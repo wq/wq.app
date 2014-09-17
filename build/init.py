20fd8b9837536eb13012886df1d2f8fcedc10f50
S. Andrew Sheppard <andrew@wq.io>
2014-09-17 21:31:31 +0000

    auto-link scss folder

--- build/init.py
+++ build/init.py
@@ -1,15 +1,21 @@
-import os
+from os.path import exists, join, dirname
+from os import symlink
 
 
 def init(conf, indir):
-    jsdir = conf.get('js', indir + '/js')
-    cssdir = conf.get('css', indir + '/css')
-    assets = os.path.dirname(os.path.dirname(__file__))
+    for name in ('js', 'css', 'scss'):
+        basedir = join(indir, conf.get(name, name))
+        if not exists(basedir):
+            continue
 
-    if not os.path.exists(jsdir + '/lib'):
-        os.symlink(assets + '/js', jsdir + '/lib')
-    elif not os.path.exists(jsdir + '/lib/wq'):
-        os.symlink(assets + '/js/wq', jsdir + '/lib/wq')
+        # Project lib directory (e.g. myapp/js/lib)
+        projpath = join(basedir, 'lib')
+        # wq source directory (e.g. wq.app/js)
+        wqpath = join(dirname(dirname(__file__)), name)
 
-    if not os.path.exists(cssdir + '/lib'):
-        os.symlink(assets + '/css', cssdir + '/lib')
+        if not exists(projpath):
+            # e.g myapp/js/lib -> wq.app/js
+            symlink(wqpath, projpath)
+        elif exists(join(wqpath, 'wq')) and not exists(join(projpath, 'wq')):
+            # e.g myapp/js/lib/wq -> wq.app/js/wq
+            symlink(join(wqpath, 'wq'), join(projpath, 'wq'))
