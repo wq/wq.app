04ac11d57f097b22e211c4ddc820631a7bbab4cb
S. Andrew Sheppard <andrew@wq.io>
2015-03-04 10:24:41 -0600

    v0.7.3

--- js/wq/appcache.js
+++ js/wq/appcache.js
@@ -1,5 +1,5 @@
 /*
- * wq.app 0.7.3-dev - wq/appcache.js
+ * wq.app 0.7.3 - wq/appcache.js
  * Monitor HTML5 appcache events and trigger callback
  * (c) 2012-2015, S. Andrew Sheppard
  * https://wq.io/license
--- js/wq/pandas.js
+++ js/wq/pandas.js
@@ -1,5 +1,5 @@
 /*
- * wq.app 0.7.3-dev - wq/pandas.js
+ * wq.app 0.7.3 - wq/pandas.js
  * Load and parse CSV with complex headers (e.g. from pandas DataFrames)
  * (c) 2014-2015, S. Andrew Sheppard
  * https://wq.io/license
