a16365f3c3a3d5d06d56acea6cc64d2463467f87
S. Andrew Sheppard <andrew@wq.io>
2016-03-10 22:29:03 -0600

    use HTTP server for tests

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,5 @@ set -e
 if [ "$LINT" ]; then
     jshint js/wq
 else
-    node-qunit-phantomjs tests/suite.html --verbos
+    node-qunit-phantomjs http://localhost:8080/tests/suite.html --verbose
 fi
