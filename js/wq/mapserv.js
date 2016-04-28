a2e424974a6b8f80626b2ddadfdcbb7269fc84a4
S. Andrew Sheppard <andrew@wq.io>
2016-04-28 11:12:11 -0500

    additional esri layer types

--- js/wq/mapserv.js
+++ js/wq/mapserv.js
@@ -5,8 +5,21 @@
  * https://wq.io/license
  */
 
-define(['wq/map', 'esri-leaflet'],
-function(map, esri) {
+define(['wq/map', 'leaflet', 'esri-leaflet'],
+function(map, L, esri) {
+
+map.addBasemapType('esri-basemap', function(conf) {
+    var layer;
+    if (conf.labels) {
+        layer = L.layerGroup([
+            esri.basemapLayer(conf.layer, conf),
+            esri.basemapLayer(conf.layer + 'Labels', conf)
+        ]);
+    } else {
+        layer = esri.basemapLayer(conf.layer, conf);
+    }
+    return layer;
+});
 
 map.addBasemapType('esri-tiled', function(conf) {
     return esri.tiledMapLayer(conf);
@@ -14,6 +27,10 @@ map.addBasemapType('esri-tiled', function(conf) {
 
 map.addOverlayType('esri-dynamic', function(conf) {
     return esri.dynamicMapLayer(conf);
+});
+
+map.addOverlayType('esri-tiled', function(conf) {
+    return esri.tiledMapLayer(conf);
 });
 
 return map;
