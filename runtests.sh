d3afb4689558003c8f6e483b2761725f79b5c379
S. Andrew Sheppard <andrew@wq.io>
2016-06-01 14:23:17 -0500

    increase timeout

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,5 @@ set -e
 if [ "$LINT" ]; then
     jshint js/wq tests/js/suite
 else
-    node-qunit-phantomjs http://localhost:8080/tests/suite.html --verbose --timeout 10
+    node-qunit-phantomjs http://localhost:8080/tests/suite.html --verbose --timeout 60
 fi
