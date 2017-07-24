b9cf701932c13d6b3aab407316d1717a6506f4ea
S. Andrew Sheppard <andrew@wq.io>
2017-07-24 12:55:32 -0500

    loosen version

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         'wq.app': package_data
     },
     install_requires=[
-        'wq.core>=0.8.1',
+        'wq.core',
         'pyScss>=1.3',
         'compass-stylesheets==0.12.6',
         'PyYAML',
