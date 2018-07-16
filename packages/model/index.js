0e80d031fb07863cd8776424a9721549f8991ce8
S. Andrew Sheppard <andrew@wq.io>
2018-07-16 16:44:15 -0500

    @wq/model

---
+++ packages/model/index.js
@@ -0,0 +1,9 @@
+/*!
+ * wq.app 2.0.0-dev - wq/model.js
+ * A simple model API for working with stored lists
+ * (c) 2012-2017, S. Andrew Sheppard
+ * https://wq.io/license
+ */
+
+var Model = require('./src/model.js');
+module.exports = Model;
