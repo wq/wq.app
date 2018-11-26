490038c124e4d168d2052199da60371f73930105
S. Andrew Sheppard <andrew@wq.io>
2018-11-26 12:49:21 +0900

    @wq/outbox

---
+++ packages/outbox/index.js
@@ -0,0 +1,9 @@
+/*!
+ * wq.app 2.0.0-dev - wq/outbox.js
+ * A simple model API for working with stored lists
+ * (c) 2012-2018, S. Andrew Sheppard
+ * https://wq.io/license
+ */
+
+var outbox = require('./src/outbox.js');
+module.exports = outbox;
