d9467a616baf4ee5b84c65c3645cba03a1111ae8
S. Andrew Sheppard <andrew@wq.io>
2018-11-28 10:58:50 +0900

    Jest env with JSDOM and (Fake)IndexedDB

---
+++ packages/jest-env-jsdom-idb/src/env.test.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment ./src/env
+ */
+
+import structuredClone from 'realistic-structured-clone';
+
+
+test("IDB in environment", () => {
+    expect(global.indexedDB).toBeTruthy();
+});
+
+test("Can clone Blob", () => {
+    const blob1 = new Blob([1,2,3], {'type': 'text/plain'});
+    const blob2 = structuredClone(blob1);
+    expect(blob1).toEqual(blob2);
+});
