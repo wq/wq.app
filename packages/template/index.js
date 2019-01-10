0a11842a64382bbbca43f86ad9de07656c7c0171
S. Andrew Sheppard <andrew@wq.io>
2019-01-11 08:38:22 +0900

    @wq/template

---
+++ packages/template/index.js
@@ -0,0 +1,9 @@
+/*!
+ * wq.app 2.0.0-dev - wq/template.js
+ * Render and inject Mustache templates
+ * (c) 2012-2019, S. Andrew Sheppard
+ * https://wq.io/license
+ */
+
+var tmpl = require('./src/template.js');
+module.exports = tmpl;
