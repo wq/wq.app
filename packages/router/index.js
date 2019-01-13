23f56d2f1334555bff8126fe2098a2532ede11ce
S. Andrew Sheppard <andrew@wq.io>
2019-01-14 08:26:26 +0900

    @wq/router.js

---
+++ packages/router/index.js
@@ -0,0 +1,9 @@
+/*!
+ * wq.app 2.0.0-dev - wq/router.js
+ * Respond to URL changes with locally generated pages and custom events
+ * (c) 2012-2019, S. Andrew Sheppard
+ * https://wq.io/license
+ */
+
+var router = require('./src/router.js');
+module.exports = router;
