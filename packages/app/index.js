da8d3c0302213287a74d15a847e6984f9c9e27a6
S. Andrew Sheppard <andrew@wq.io>
2019-01-17 16:35:44 +0900

    @wq/app

---
+++ packages/app/index.js
@@ -0,0 +1,10 @@
+/*!
+ * wq.app 2.0.0-dev - wq/app.js
+ * Utilizes @wq/store and @wq/pages to dynamically load and render content from
+ * a wq.db-compatible REST service
+ * (c) 2012-2019, S. Andrew Sheppard
+ * https://wq.io/license
+ */
+
+var app = require('./src/app.js');
+module.exports = app;
--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,5 @@ else
     cd tests/commands;
     ./test_commands.sh
     cd ../..;
-    # FIXME: Restore or migrate tests
-    # node-qunit-phantomjs http://localhost:8080/tests/suite.html --verbose --timeout 60
     npm test
 fi
