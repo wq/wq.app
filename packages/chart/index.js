829d2e0d32fd4035d09245185551e5bbb67c18d6
S. Andrew Sheppard <andrew@wq.io>
2019-04-22 13:44:47 +0900

    @wq/chart

---
+++ packages/chart/index.js
@@ -0,0 +1,9 @@
+/*!
+ * wq.app 2.0.0-dev - wq/chart.js
+ * d3-powered CSV charts for @wq/app pages
+ * (c) 2013-2019, S. Andrew Sheppard
+ * https://wq.io/license
+ */
+
+var chart = require('./src/chart.js');
+module.exports = chart;
