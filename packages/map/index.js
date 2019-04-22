730137549a3d53fa9639d2d2d3e8e963172c9510
S. Andrew Sheppard <andrew@wq.io>
2019-04-22 10:19:23 +0900

    @wq/map.js

---
+++ packages/map/index.js
@@ -0,0 +1,9 @@
+/*!
+ * wq.app 2.0.0-dev - wq/map.js
+ * Leaflet integration for @wq/app pages
+ * (c) 2013-2019, S. Andrew Sheppard
+ * https://wq.io/license
+ */
+
+var map = require('./src/map.js');
+module.exports = map;
