1f66caa13664fbd4464584ca69792ed4fc17959e
S. Andrew Sheppard <andrew@wq.io>
2019-11-14 14:43:36 +0900

    export components

--- packages/material/index.js
+++ packages/material/index.js
@@ -1,2 +1,29 @@
 import material from './src/index.js';
+import App from './src/App.js';
+import {
+    Container,
+    Header,
+    Main,
+    Footer,
+    Spinner,
+    Link,
+    ButtonLink,
+    ListItemLink,
+    Breadcrumbs,
+    Pagination
+} from './src/components/index.js';
+
 export default material;
+export {
+    App,
+    Container,
+    Header,
+    Main,
+    Footer,
+    Spinner,
+    Link,
+    ButtonLink,
+    ListItemLink,
+    Breadcrumbs,
+    Pagination
+};
