15f0c0f824a041733cada279caee4bf86932e902
S. Andrew Sheppard <andrew@wq.io>
2020-02-21 11:40:24 +0900

    loose deps for dev builds

--- set_dev_version.sh
+++ set_dev_version.sh
@@ -6,4 +6,4 @@ VERSION=`python3 setup.py --version | \
         sed "s/^\([0-9]\+\.[0-9]\+\.[0-9]\+\)\(.\+\)/\1-\2/" | \
         sed "s/--/-/g"`
 sed -i "s/\"version\": .*/\"version\": \"$VERSION\",/" packages/*/package.json
-sed -i "s/\"@wq\/\(.\+\)\": \".\+\"/\"@wq\/\1\": \"\^$VERSION\"/" packages/*/package.json
+sed -i "s/\"@wq\/\(.\+\)\": \".\+\"/\"@wq\/\1\": \"*\"/" packages/*/package.json
