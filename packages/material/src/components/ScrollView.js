c7da0872bb1f7b2a481209bf2be88c7ec87bb675
S. Andrew Sheppard <andrew@wq.io>
2020-04-10 17:07:15 +0900

    preserve custom style

--- packages/material/src/components/ScrollView.js
+++ packages/material/src/components/ScrollView.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function ScrollView({ children }) {
+export default function ScrollView({ children, style }) {
     return (
-        <div style={{ overflowX: 'hidden', overflowY: 'auto', flex: 1 }}>
+        <div
+            style={{
+                overflowX: 'hidden',
+                overflowY: 'auto',
+                flex: 1,
+                ...style
+            }}
+        >
             {children}
         </div>
     );
 }
 
 ScrollView.propTypes = {
-    children: PropTypes.node
+    children: PropTypes.node,
+    style: PropTypes.object
 };
--- packages/material/src/components/ScrollView.native.js
+++ packages/material/src/components/ScrollView.native.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { ScrollView as PaperScrollView } from 'react-native';
 import { useTheme } from 'react-native-paper';
+import PropTypes from 'prop-types';
 
-export default function ScrollView(props) {
+export default function ScrollView({ style, ...rest }) {
     const theme = useTheme();
     return (
         <PaperScrollView
-            style={{ flex: 1, backgroundColor: theme.colors.background }}
-            {...props}
+            style={{
+                flex: 1,
+                backgroundColor: theme.colors.background,
+                ...style
+            }}
+            {...rest}
         />
     );
 }
+
+ScrollView.propTypes = {
+    style: PropTypes.object
+};
