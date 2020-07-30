7fe15abaa9f8542b58512475e7ebb21d0abbbe59
S. Andrew Sheppard <andrew@wq.io>
2020-07-30 13:37:37 +0900

    expo can use @react-native-community/async-storage

--- packages/outbox/src/offline.js
+++ packages/outbox/src/offline.js
@@ -1,29 +1,7 @@
+import { createOffline } from '@redux-offline/redux-offline';
+import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 import { RESET_STATE } from '@redux-offline/redux-offline/lib/constants';
 import { busy } from '@redux-offline/redux-offline/lib/actions';
 
-import detectNetwork from '@redux-offline/redux-offline/lib/defaults/detectNetwork';
-import retry from '@redux-offline/redux-offline/lib/defaults/retry';
-import discard from '@redux-offline/redux-offline/lib/defaults/discard';
-import defaultCommit from '@redux-offline/redux-offline/lib/defaults/defaultCommit';
-import defaultRollback from '@redux-offline/redux-offline/lib/defaults/defaultRollback';
-import offlineStateLens from '@redux-offline/redux-offline/lib/defaults/offlineStateLens';
-
-const createOffline = require('../vendor/redux-offline').createOffline;
-
-// Simulate default config but skip importing unused items
-const offlineConfig = {
-    rehydrate: true,
-    // persist,
-    detectNetwork,
-    // effect,
-    retry,
-    discard,
-    defaultCommit,
-    defaultRollback,
-    // persistAutoRehydrate,
-    offlineStateLens,
-    // queue,
-    returnPromises: false
-};
 
 export { createOffline, offlineConfig, RESET_STATE, busy };
