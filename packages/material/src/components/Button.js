a2e30c303b640f976f892ea500282de04ae87a33
S. Andrew Sheppard <andrew@wq.io>
2020-08-07 12:33:40 +0900

    useIconComponents for Button

--- packages/material/src/components/Button.js
+++ packages/material/src/components/Button.js
@@ -1,3 +1,14 @@
-import Button from '@material-ui/core/Button';
+import React from 'react';
+import MuiButton from '@material-ui/core/Button';
+import { useIconComponents } from '@wq/react';
+import PropTypes from 'prop-types';
 
-export default Button;
+export default function Button({ icon, ...rest }) {
+    const { [icon]: Icon } = useIconComponents(),
+        startIcon = Icon ? <Icon /> : null;
+    return <MuiButton startIcon={startIcon} {...rest} />;
+}
+
+Button.propTypes = {
+    icon: PropTypes.string
+};
--- packages/material/src/components/Button.native.js
+++ packages/material/src/components/Button.native.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { useIconComponents } from '@wq/react';
 import { Button as PaperButton } from 'react-native-paper';
 import PropTypes from 'prop-types';
 
-export default function Button({ onClick, onPress, variant, mode, ...rest }) {
+export default function Button({
+    onClick,
+    onPress,
+    variant,
+    mode,
+    icon,
+    ...rest
+}) {
+    const { [icon]: Icon } = useIconComponents();
     if (!onPress) {
         onPress = onClick;
     }
     if (!mode) {
         mode = variant;
     }
-    return <PaperButton onPress={onPress} mode={mode} {...rest} />;
+    return <PaperButton onPress={onPress} mode={mode} icon={Icon} {...rest} />;
 }
 
 Button.propTypes = {
     onClick: PropTypes.func,
     onPress: PropTypes.func,
     variant: PropTypes.string,
-    mode: PropTypes.string
+    mode: PropTypes.string,
+    icon: PropTypes.string
 };
