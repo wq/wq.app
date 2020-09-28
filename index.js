392587e0917f42a5e625941f837fcdedf378b3ae
S. Andrew Sheppard <andrew@wq.io>
2020-09-28 11:49:21 +0900

    export third party libs for custom integration

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import app from '@wq/app';
 import material from '@wq/material';
 import mapbox from '@wq/mapbox';
 
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+
 app.use([material, mapbox]);
 
 export default app;
+
+export { React, mapboxgl };
