7cf9aaf7e413c653c53888b687739b750bf9381a
S. Andrew Sheppard <andrew@wq.io>
2021-03-15 03:01:07 -0500

    support ListItem secondaryAction

--- packages/material/src/components/ListItem.js
+++ packages/material/src/components/ListItem.js
@@ -2,10 +2,17 @@ import React from 'react';
 import MuiListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import PropTypes from 'prop-types';
 import { useIcon } from '@wq/react';
 
-export default function ListItem({ children, description, icon, ...rest }) {
+export default function ListItem({
+    children,
+    description,
+    icon,
+    secondaryAction,
+    ...rest
+}) {
     const Icon = useIcon(icon);
     return (
         <MuiListItem {...rest}>
@@ -15,6 +22,11 @@ export default function ListItem({ children, description, icon, ...rest }) {
                 </ListItemIcon>
             )}
             <ListItemText primary={children} secondary={description} />
+            {secondaryAction && (
+                <ListItemSecondaryAction>
+                    {secondaryAction}
+                </ListItemSecondaryAction>
+            )}
         </MuiListItem>
     );
 }
@@ -22,5 +34,6 @@ export default function ListItem({ children, description, icon, ...rest }) {
 ListItem.propTypes = {
     children: PropTypes.node,
     description: PropTypes.node,
-    icon: PropTypes.string
+    icon: PropTypes.string,
+    secondaryAction: PropTypes.node
 };
