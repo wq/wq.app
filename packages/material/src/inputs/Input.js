7bfef38ab7b863d6f9eee6811a4e55e4dbbb0d2c
S. Andrew Sheppard <andrew@wq.io>
2021-03-16 08:33:04 -0500

    correctly apply maxLength prop

--- packages/material/src/inputs/Input.js
+++ packages/material/src/inputs/Input.js
@@ -4,21 +4,24 @@ import { TextField } from 'formik-material-ui';
 import { useHtmlInput } from '@wq/react';
 import PropTypes from 'prop-types';
 
-export default function Input({ hint, ...rest }) {
-    const inputProps = useHtmlInput(rest);
+export default function Input({ hint, inputProps, ...rest }) {
+    const { name, type, maxLength } = useHtmlInput(rest);
     return (
         <Field
+            name={name}
+            type={type}
             fullWidth
             margin="dense"
             component={TextField}
             helperText={hint}
+            inputProps={{ maxLength, ...inputProps }}
             {...rest}
-            {...inputProps}
         />
     );
 }
 
 Input.propTypes = {
     name: PropTypes.string,
-    hint: PropTypes.string
+    hint: PropTypes.string,
+    inputProps: PropTypes.object
 };
