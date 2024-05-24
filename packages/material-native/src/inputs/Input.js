cc7c340484d57c2737d339508ceac9004c0727e8
S. Andrew Sheppard <andrew@wq.io>
2024-05-24 13:55:37 -0400

    support decimal entry with step

--- packages/material-native/src/inputs/Input.js
+++ packages/material-native/src/inputs/Input.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import { TextInput } from "react-native-paper";
 import { useField } from "formik";
@@ -14,13 +14,22 @@ const keyboards = {
 };
 
 export default function Input(props) {
-    const { name, type, label, hint, style, min, max } = props,
+    const { name, type, label, hint, style, min, max, step } = props,
         { maxLength } = useHtmlInput(props),
         [, meta, helpers] = useField(name),
         { value } = meta,
-        { setValue, setTouched } = helpers;
+        [formatValue, setFormatValue] = useState(
+            type === "int" || type === "decimal"
+                ? typeof value === "number"
+                    ? "" + value
+                    : ""
+                : value
+        ),
+        { setValue, setTouched } = helpers,
+        [decimal, setDecimal] = useState();
 
-    function handleChange(value) {
+    function handleChange(nextValue) {
+        let value = nextValue;
         if (type === "int" || type === "decimal") {
             if (type === "int") {
                 value = parseInt(value);
@@ -29,6 +38,7 @@ export default function Input(props) {
             }
             if (Number.isNaN(value)) {
                 setValue(null);
+                setFormatValue("");
             } else {
                 if (typeof min === "number" && value < min) {
                     value = min;
@@ -36,18 +46,20 @@ export default function Input(props) {
                 if (typeof max === "number" && value > max) {
                     value = max;
                 }
+                if (step) {
+                    value = +(Math.round(value / step) * step).toFixed(
+                        step < 1 ? step.toString().split(".")[1].length : 0
+                    );
+                }
                 setValue(value);
+                setFormatValue(
+                    +nextValue === value ? nextValue : value.toString()
+                );
             }
         } else {
             setValue(value);
+            setFormatValue(value);
         }
-    }
-
-    let formatValue;
-    if (type === "int" || type === "decimal") {
-        formatValue = typeof value === "number" ? "" + value : "";
-    } else {
-        formatValue = value;
     }
 
     return (
